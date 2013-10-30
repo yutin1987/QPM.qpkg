0ddf56077e36727630c8bdd20948b6026fdc85dd
Justin <yuting1987@gmail.com>
2013-10-30 17:16:46 +0800

    start

--- share/web/download.php
+++
@@ -1,5 +0,0 @@
-<?php
-	header('Content-type:application/force-download');
-	header('Content-Transfer-Encoding: Binary');
-	header('Content-Disposition:attachment;filename=image2pdf.qpkg');
-	@readfile('./image2pdf.qpkg');
